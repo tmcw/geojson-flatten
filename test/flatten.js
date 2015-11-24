572712d7daa8ec3355927d763fce9e27d21a0841
Tom MacWright <tom@macwright.org>
2015-11-24 16:34:47 -0500

    Update test strategy

--- test/flatten.js
+++ test/flatten.js
@@ -2,16 +2,24 @@ var flatten = require('../'),
     fs = require('fs'),
     test = require('tap').test;
 
-function f(_) {
-    return JSON.parse(fs.readFileSync(_, 'utf8'));
-}
-
-test('flatten', function(t) {
-    t.deepEqual(flatten(f('./test/multigeometry.input.geojson')), f('./test/multigeometry.output.geojson'),
-        'expands geometrycollection');
-    t.deepEqual(flatten(f('./test/point.input.geojson')), f('./test/point.input.geojson'),
-        'does not touch point');
-    t.deepEqual(flatten(f('./test/linestring.geojson')), f('./test/linestring.geojson'),
-        'does not touch line');
-    t.end();
+test('flatten', function(group) {
+    fs.readdirSync(__dirname + '/fixture')
+      .filter(function(fix) { return fix.match(/input/); })
+      .forEach(function(fixture) {
+        group.test(fixture, function(t) {
+          var result = flatten(
+            JSON.parse(
+              fs.readFileSync(__dirname + '/fixture/' + fixture)));
+          if (process.env.UPDATE) {
+            fs.writeFileSync(__dirname + '/fixture/' + fixture.replace('input', 'output'),
+              JSON.stringify(result, null, 2));
+          }
+          t.deepEqual(result,
+            JSON.parse(
+              fs.readFileSync(
+                __dirname + '/fixture/' + fixture.replace('input', 'output'))));
+           t.end();
+        });
+    });
+    group.end();
 });
