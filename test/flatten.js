c7e9585e05c57fc90fc09d982e2786ce8c140747
Tom MacWright <tom@macwright.org>
2019-01-28 23:31:16 -0800

    refactor: Modernize, use standard-version

--- test/flatten.js
+++ test/flatten.js
@@ -1,25 +1,33 @@
-var flatten = require('../'),
-    fs = require('fs'),
-    test = require('tap').test;
+const flatten = require("../"),
+  fs = require("fs"),
+  test = require("tap").test;
 
-test('flatten', function(group) {
-    fs.readdirSync(__dirname + '/fixture')
-      .filter(function(fix) { return fix.match(/input/); })
-      .forEach(function(fixture) {
-        group.test(fixture, function(t) {
-          var result = flatten(
-            JSON.parse(
-              fs.readFileSync(__dirname + '/fixture/' + fixture)));
-          if (process.env.UPDATE) {
-            fs.writeFileSync(__dirname + '/fixture/' + fixture.replace('input', 'output'),
-              JSON.stringify(result, null, 2));
-          }
-          t.deepEqual(result,
-            JSON.parse(
-              fs.readFileSync(
-                __dirname + '/fixture/' + fixture.replace('input', 'output'))));
-           t.end();
-        });
+test("flatten", function(group) {
+  fs.readdirSync(__dirname + "/fixture")
+    .filter(function(fix) {
+      return fix.match(/input/);
+    })
+    .forEach(function(fixture) {
+      group.test(fixture, function(t) {
+        const result = flatten(
+          JSON.parse(fs.readFileSync(__dirname + "/fixture/" + fixture))
+        );
+        if (process.env.UPDATE) {
+          fs.writeFileSync(
+            __dirname + "/fixture/" + fixture.replace("input", "output"),
+            JSON.stringify(result, null, 2)
+          );
+        }
+        t.deepEqual(
+          result,
+          JSON.parse(
+            fs.readFileSync(
+              __dirname + "/fixture/" + fixture.replace("input", "output")
+            )
+          )
+        );
+        t.end();
+      });
     });
-    group.end();
+  group.end();
 });
